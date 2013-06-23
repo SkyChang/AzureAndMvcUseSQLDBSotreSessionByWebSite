9d98d5223ecf084896fc3f45c2e874e9bc2b3d94
SkyChang <sanc.lcdbest+iservice@gmail.com>
2013-06-23 19:00:04 +0800

    SkyChang 0623 First Commit

---
+++ AzureAndMvcUseSQLDBSotreSessionByWebSite/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureAndMvcUseSQLDBSotreSessionByWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Session["Test"] = "Have Value";
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+    }
+}
